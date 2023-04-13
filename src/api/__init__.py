5b1f87bed14f517c97b853d6b0a44e5d79c83a09
ZWForum <782552186@qq.com>
2023-04-13 17:36:44 +0800

    init

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -9,6 +9,7 @@ method_get = {}
 method_post = {}
 for (k, v) in method_get.items():
     api_blueprint.add_route(v, k)
+    logger.info(f'API GET {k} 注册成功')
 for (k, v) in method_post.items():
     api_blueprint.add_route(v, k, methods=['POST'])
-logger.info('蓝图注册成功')
+    logger.info(f'API POST {k} 注册成功')
