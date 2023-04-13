d17a9ad2ae811666756c3bb75df8fbbfade3aed5
ZWForum <782552186@qq.com>
2023-04-13 21:20:29 +0800

    init

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,8 +1,23 @@
+import os
+import re
+import sys
+
 import config
 from src.utils.logger import get_logger
 from src.utils.model import ModelExt
 
 logger = get_logger(__name__)
+if hasattr(config, 'USE_GPU'):
+    from config import USE_GPU
+
+    for dir_name in os.listdir(os.path.dirname(os.path.realpath('src'))):
+        if re.match(r'openpose.*', dir_name):
+            build_dir = 'build_GPU' if USE_GPU else 'build_CPU'
+            bin_dir = os.path.join(dir_name, build_dir, 'bin')
+            dll_dir = os.path.join(dir_name, build_dir, 'x64', 'Release')
+            os.environ['PATH'] = os.environ['PATH'] + ';' + bin_dir + ';' + dll_dir
+            sys.path.append(os.path.join(dir_name, build_dir, 'python', 'openpose', 'Release'))
+            break
 if hasattr(config, 'DATABASE_CONFIG') and hasattr(config, 'TABLES'):
     from sqlalchemy import create_engine, MetaData
     from sqlalchemy.ext.automap import automap_base
