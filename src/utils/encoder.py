e6869a1b87216bafafa0085ba0368cee0d159d13
ZWForum <782552186@qq.com>
2023-04-13 17:51:47 +0800

    init

---
+++ src/utils/encoder.py
@@ -0,0 +1,22 @@
+import json
+
+from typing import List
+
+
+class JSONEncoder(json.JSONEncoder):
+    """
+    自定义JSON序列化编码器
+    """
+    ext: List[tuple] = None
+
+    def default(self, o):
+        """
+        处理序列化类型
+
+        :param o: 待序列化数据
+        :return: Any
+        """
+        for item in JSONEncoder.ext:
+            if isinstance(o, item[0]):
+                return item[1](o)
+        return super(JSONEncoder, self).default(o)
