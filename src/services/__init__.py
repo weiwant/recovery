bf96342c0a1f5d9cc53ff4958e1724924f36ee05
ZWForum <782552186@qq.com>
2023-04-13 19:59:45 +0800

    init

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -1,7 +1,7 @@
 from logging import getLogger, StreamHandler
 
 import config
-from src.utils.model import ModelExt
+from src.models.model import ModelExt
 
 logger = getLogger(__name__)
 logger.setLevel('INFO')
