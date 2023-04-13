9c3d98d68333285e2fa539e2a4a95cc858ac77c7
ZWForum <782552186@qq.com>
2023-04-13 17:24:27 +0800

    init

---
+++ config/__init__.py
@@ -0,0 +1,12 @@
+DATABASE_CONFIG = {
+    'dbtype': 'mysql',
+    'user': 'Manager',
+    'host': 'localhost',
+    'password': '123456',
+    'driver': 'pymysql',
+    'dbname': 'recovery',
+    'port': '3306'
+}
+TABLES = [
+    'test'
+]
