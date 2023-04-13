8208636b53d4e1f476ecdc0e179618b59dce69f5
ZWForum <782552186@qq.com>
2023-04-13 21:15:34 +0800

    init

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -2,6 +2,12 @@ from logging import getLogger, StreamHandler, Formatter
 
 
 def get_logger(name):
+    """
+    获取日志记录器
+
+    :param name: 日志记录器名称
+    :return: 日志记录器
+    """
     logger = getLogger(name)
     logger.setLevel('INFO')
     console = StreamHandler()
