58a3b9c0e24839c67486d21888a126fbbe067150
ZWForum <782552186@qq.com>
2023-04-13 17:30:03 +0800

    init

--- src/services/__init__.py
+++ src/services/__init__.py
@@ -2,6 +2,7 @@ from logging import getLogger
 
 import config
 
+logger = getLogger(__name__)
 if hasattr(config, 'DATABASE_CONFIG') and hasattr(config, 'TABLES'):
     from sqlalchemy import create_engine, MetaData
     from sqlalchemy.ext.automap import automap_base
@@ -10,7 +11,6 @@ if hasattr(config, 'DATABASE_CONFIG') and hasattr(config, 'TABLES'):
     from config import DATABASE_CONFIG, TABLES
     from src.utils.database import get_connect_string
 
-    logger = getLogger(__name__)
     connect_string = get_connect_string(**DATABASE_CONFIG)
     engine = create_engine(connect_string)
     meta = MetaData()
