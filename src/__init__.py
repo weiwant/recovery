a2c82dfcd0c775dc8957c6343daf2dd77a12a1d2
ZWForum <782552186@qq.com>
2023-04-13 17:36:34 +0800

    init

---
+++ src/__init__.py
@@ -0,0 +1,19 @@
+from logging import getLogger
+
+import sanic
+from sanic_cors import CORS
+
+from src.api import api_blueprint
+from src.middlewares import REQUEST, RESPONSE
+
+logger = getLogger(__name__)
+app = sanic.Sanic(__name__)
+app.blueprint(api_blueprint)
+for middleware in REQUEST:
+    app.on_request(middleware)
+    logger.info(f'请求中间件{middleware.__name__}注册成功')
+for middleware in RESPONSE:
+    app.on_response(middleware)
+    logger.info(f'响应中间件{middleware.__name__}注册成功')
+CORS(app)
+logger.info('CORS注册成功')
