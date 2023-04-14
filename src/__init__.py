2a59c2c28f650f6748e4d21929b818f9fe01a2a8
ZWForum <782552186@qq.com>
2023-04-14 21:07:12 +0800

    init

--- src/__init__.py
+++ src/__init__.py
@@ -1,13 +1,13 @@
 import sanic
 from sanic_cors import CORS
 
-from src.api import api_blueprint
+from src.api import api_blueprint, api_group
 from src.middlewares import REQUEST, RESPONSE
 from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
 app = sanic.Sanic(__name__)
-app.blueprint(api_blueprint)
+app.blueprint(sanic.Blueprint.group(api_blueprint, api_group))
 for middleware in REQUEST:
     app.on_request(middleware)
     logger.info(f'请求中间件{middleware.__name__}注册成功')
--- src/api/__init__.py
+++ src/api/__init__.py
@@ -5,11 +5,16 @@ from src.utils.logger import get_logger
 logger = get_logger(__name__)
 
 api_blueprint = sanic.blueprints.Blueprint('api', url_prefix='/recovery/api')
+api_group = sanic.blueprints.BlueprintGroup(url_prefix='/recovery/api')
 method_get = {}
 method_post = {}
+blueprints = []
 for (k, v) in method_get.items():
     api_blueprint.add_route(v, k)
     logger.info(f'API GET {k} 注册成功')
 for (k, v) in method_post.items():
     api_blueprint.add_route(v, k, methods=['POST'])
     logger.info(f'API POST {k} 注册成功')
+for blueprint in blueprints:
+    api_group.append(blueprint)
+    logger.info(f'API Group {blueprint.name} 注册成功')
