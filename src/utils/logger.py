a1a477a3305b2b310e886613e1bc1c912cec8cf4
ZWForum <782552186@qq.com>
2023-04-14 15:50:27 +0800

    init

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -11,6 +11,6 @@ def get_logger(name):
     logger = getLogger(name)
     logger.setLevel('INFO')
     console = StreamHandler()
-    console.setFormatter(Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s'))
+    console.setFormatter(Formatter('[%(asctime)s] - %(name)s - %(levelname)s - %(message)s'))
     logger.addHandler(console)
     return logger
