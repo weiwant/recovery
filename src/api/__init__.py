b2d3daa8fb63412de4669f9f5b9c4b346a69b1f3
ZWForum <782552186@qq.com>
2023-04-17 03:18:28 +0800

    init

--- src/api/__init__.py
+++ src/api/__init__.py
@@ -1,5 +1,6 @@
 import sanic
 
+from src.api.user import user_blueprint
 from src.utils.logger import get_logger
 
 logger = get_logger(__name__)
@@ -8,7 +9,9 @@ api_blueprint = sanic.blueprints.Blueprint('api', url_prefix='/recovery/api')
 api_group = sanic.blueprints.BlueprintGroup(url_prefix='/recovery/api')
 method_get = {}
 method_post = {}
-blueprints = []
+blueprints = [
+    user_blueprint
+]
 for (k, v) in method_get.items():
     api_blueprint.add_route(v, k)
     logger.info(f'API GET {k} 注册成功')
