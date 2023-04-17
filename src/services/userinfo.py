fba16a5d04a457c673684ebc931933c0a2bbef0c
ZWForum <782552186@qq.com>
2023-04-17 11:54:22 +0800

    init

--- src/services/userinfo.py
+++ src/services/userinfo.py
@@ -1,5 +1,3 @@
-from Crypto.Hash import MD5
-
 from src.models.mapper import Mapper
 from src.models.model import DataModel
 from src.services import session_maker, base
@@ -12,44 +10,3 @@ fields = Mapper({
     'password': 'password',
     'type': 'type'
 })
-
-
-def register(**kwargs):
-    """
-    注册
-
-    :param kwargs:
-    :return:
-    """
-    args = ['username', 'password', 'type']
-    if fields.has_values(kwargs, args):
-        args = fields.get_dict(kwargs, args)
-        args[fields.map_dict['password']] = MD5.new(args[fields.map_dict['password']].encode('utf-8')).hexdigest()
-        result = user_info.add_record(**args)
-        return 200 if result is not None else 500, result
-    else:
-        logger.error('缺少参数')
-        return 500, None
-
-
-def login(**kwargs):
-    """
-    登录
-
-    :param kwargs:
-    :return:
-    """
-    args = ['username', 'password']
-    if fields.has_values(kwargs, args):
-        args = fields.get_dict(kwargs, args)
-        args[fields.map_dict['password']] = MD5.new(args[fields.map_dict['password']].encode('utf-8')).hexdigest()
-        result = user_info.get_record(**args)
-        if result is None:
-            return 500, None
-        elif not result:
-            return 401, None
-        else:
-            return 200, result[0].to_json()
-    else:
-        logger.error('缺少参数')
-        return 500, None
