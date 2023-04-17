310e4811b2ba4839dd58723f96da5769d39540d0
ZWForum <782552186@qq.com>
2023-04-17 13:55:44 +0800

    init

--- src/models/error.py
+++ src/models/error.py
@@ -40,3 +40,25 @@ class RuleError(Exception):
         :return:
         """
         return self.message
+
+
+class DataError(Exception):
+    """
+    数据错误
+    """
+
+    def __init__(self, message):
+        """
+        初始化
+
+        :param message: 错误信息
+        """
+        self.message = message
+
+    def __str__(self):
+        """
+        字符串化
+
+        :return:
+        """
+        return self.message
