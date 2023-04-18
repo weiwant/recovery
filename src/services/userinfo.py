3063877748525807b4ef67eb0c6721a7697cb10a
ZWForum <782552186@qq.com>
2023-04-18 17:09:26 +0800

    init

--- src/services/userinfo.py
+++ src/services/userinfo.py
@@ -2,8 +2,6 @@ from src.models.mapper import Mapper
 from src.models.model import DataModel
 from src.services import session_maker, base
 
-user_info = DataModel(__name__, base.classes['userinfo'], session_maker, 'id')
-logger = user_info.logger
 fields = Mapper({
     'id': 'id',
     'username': 'username',
@@ -12,3 +10,5 @@ fields = Mapper({
     'iv': 'iv',
     'login_host': 'loginhost'
 })
+user_info = DataModel(__name__, base.classes['userinfo'], session_maker, fields.map_dict['id'])
+logger = user_info.logger
