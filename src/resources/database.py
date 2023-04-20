648a63e7b8138cdc89c1cabe9021be9e2ad15670
ZWForum <782552186@qq.com>
2023-04-20 14:27:33 +0800

    init

--- src/resources/database.py
+++ src/resources/database.py
@@ -5,14 +5,28 @@ from src.classes.model import DataModel
 from src.resources import session_maker, base
 
 
+class MetaClass(type):
+    """
+    元类
+    """
+
+    def __new__(mcs, name, bases, attrs):
+        """
+        创建类
+
+        :param mcs: 元类
+        :param name: 类名
+        :param bases: 父类
+        :param attrs: 属性
+        :return:
+        """
+        attrs.update({pascalcase(name): DataModel(name, base.classes[name], session_maker, key_column)
+                      for name, key_column in TABLES.items()})
+        return super().__new__(mcs, name, bases, attrs)
+
+
 class Tables:
     """
     数据表
     """
-
-    def __init__(self):
-        """
-        初始化
-        """
-        self.__dict__.update({pascalcase(name): DataModel(name, base.classes[name], session_maker, key_column)
-                              for name, key_column in TABLES.items()})
+    __metaclass__ = MetaClass
