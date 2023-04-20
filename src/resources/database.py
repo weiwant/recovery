175f7457bb76f460ec0920335a95c6e370a9cd86
ZWForum <782552186@qq.com>
2023-04-20 23:21:42 +0800

    init

--- src/resources/database.py
+++ src/resources/database.py
@@ -1,4 +1,8 @@
+from typing import Union
+
 from casestyle import pascalcase
+from pydantic import BaseModel
+from pydantic.typing import NoneType
 
 from config import TABLES
 from src.classes.model import DataModel
@@ -14,7 +18,6 @@ class MetaClass(type):
         """
         创建类
 
-        :param mcs: 元类
         :param name: 类名
         :param bases: 父类
         :param attrs: 属性
@@ -22,6 +25,13 @@ class MetaClass(type):
         """
         attrs.update({pascalcase(name): DataModel(name, base.classes[name], session_maker, key_column)
                       for name, key_column in TABLES.items()})
+        for name in TABLES.keys():
+            table = base.classes[name].__dict__['__table__']
+            annotations = {}
+            for c in getattr(table.columns, '_all_columns'):
+                annotations.update({c.name: Union[NoneType, c.type.python_type]})
+            field_name = pascalcase(name) + 'Field'
+            attrs.update({field_name: type(field_name, (BaseModel,), {'__annotations__': annotations})})
         return super().__new__(mcs, name, bases, attrs)
 
 
