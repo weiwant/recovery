bd7c1d16b31e79eb8df3c432233625b91ec6d0e9
ZWForum <782552186@qq.com>
2023-04-22 02:27:58 +0800

    init

---
+++ src/services/model.py
@@ -0,0 +1,10 @@
+def inference(video, training_root, evaluate_root):
+    """
+    推理
+
+    :param video: 视频名称
+    :param training_root: 训练文件夹
+    :param evaluate_root: 评价文件夹
+    :return:
+    """
+    return 1.0, './static/1.jpg'
