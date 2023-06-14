9c3370fb37152258f6fa230d8df2511936fcb47a
ZWForum <782552186@qq.com>
2023-06-14 19:15:32 +0800

    init

--- src/utils/pose.py
+++ src/utils/pose.py
@@ -3,22 +3,17 @@ import os
 import cv2
 import pyopenpose as op
 
-from config import OPENPOSE_ROOT
-from src.utils.logger import get_logger
 
-logger = get_logger(__name__)
-
-
-def process_video(video_path, model_type='BODY_25', max_people=1):
+def process_video(video_path, OPENPOSE_ROOT, model_type='BODY_25', max_people=1):
     """
     处理视频
 
+    :param OPENPOSE_ROOT: openpose根目录
     :param video_path: 视频路径
     :param model_type: 模型类型
     :param max_people: 最大人数
     :return:
     """
-    logger.info(f'开始处理视频: {os.path.basename(video_path)}')
     params = dict()
     params["model_folder"] = os.path.join(OPENPOSE_ROOT, 'models')
     params["model_pose"] = model_type
@@ -28,7 +23,6 @@ def process_video(video_path, model_type='BODY_25', max_people=1):
     cap = VideoCapture(video_path)
     frame_count = cap.get(getattr(cv2, 'CAP_PROP_FRAME_COUNT'))
     if not cap.isOpened():
-        logger.error(f'视频打开失败: {video_path}')
         return
     opWrapper = op.WrapperPython()
     opWrapper.configure(params)
