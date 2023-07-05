455b47d75ea1f083a7073e3daca39c8d28989cb0
wwq <1980409739@qq.com>
2023-07-05 11:21:47 +0800

    资讯前端

--- pages/article/index.js
+++ pages/article/index.js
@@ -1,4 +1,6 @@
 // pages/article/index.js
+const baseUrl="http://1636fb15.r1.cpolar.top/recovery/api"
+
 Page({
 
   /**
@@ -6,21 +8,49 @@ Page({
    */
   data: {
     newsId:0,
-    isArticle:1,
-    url:'https://s2.loli.net/2023/04/15/OoIvMjgVNFQGqBW.png',
-    // url:'http://47.113.202.168/demo.mp4',
-    title:'脑卒中，一种在中国发病率极高的疾病。',
-    content:'&emsp;&emsp;脑卒中（stroke），是指突然发生的、由脑血管病变引起的局限的或全脑的功能障碍，持续时间超过24h或引起死亡的临床综合征。我们需要知道的关于脑卒中的一些危险因素。a可调控因素：高血压、糖尿病、心脏病、高脂血症等b.可改变的因素：不良饮食习惯、大量饮酒、吸烟等c.不可变的因素：年龄、性别、家族史等\n&emsp;&emsp;脑卒中，一种在中国发病率极高的疾病。中国人每5人中就会有两人罹患脑卒中，脑卒中也是我国疾病所致寿命损失年的第一病因，每年约190余万人因卒中死亡。卒中的致残率也同样很高，高达70%-80%。那关于脑卒中我们应该知道哪些东西呢？\n&emsp;&emsp;我们需要知道的关于脑卒中的一些危险因素:\n&emsp;&emsp;a可调控因素：高血压、糖尿病、心脏病、高脂血症等\n&emsp;&emsp;b.可改变的因素：不良饮食习惯、大量饮酒、吸烟等\n&emsp;&emsp;c.不可变的因素：年龄、性别、家族史等'
+    isArticle:0,
+    url:'',
+    author:'',
+    createTime:'',
+    title:'',
+    content:''
+
+    
+  },
+  getDetail(){
+    
+    wx.request({
+      url: baseUrl+'/article/get_detail',
+      method:'POST',
+      data:{
+        id:this.data.newsId
+      },
+      success:(res)=>{
+        //console.log(res.data[0])
+        this.setData({
+          isArticle:res.data[0].type,
+          url:res.data[0].url,
+          author:res.data[0].author,
+          createTime:res.data[0].create_time,
+          title:res.data[0].title,
+          content:res.data[0].content
+        })
+      }
+    })
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    //console.log(options)
     this.setData({
       newsId:options.id,
     });
-    console.log(this.data.newsId);
+    this.getDetail()
+
+    
   },
 
   /**
--- pages/news/index.js
+++ pages/news/index.js
@@ -1,84 +1,98 @@
 // pages/news/index.js
+const baseUrl="http://1636fb15.r1.cpolar.top/recovery/api"
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
+    class:['科普','训练','药物','其他'],
     TabCur: 0,
     scrollLeft:0,
     index:0,
-    news:[
-      {
-        newsId:1,
-        title:'脑卒中，一种在中国发病率极高的疾病。',
-        imgUrl:'https://s2.loli.net/2023/04/15/OoIvMjgVNFQGqBW.png',
-        content:'脑卒中（stroke），又称脑血管意外（CVA），是指突然发生的、由脑血管病变引起的局限的或全脑的功能障碍，持续时间超过24h或引起死亡的临床综合征。我们需要知道的关于脑卒中的一些危险因素。a可调控因素：高血压、糖尿病、心脏病、高脂血症等b.可改变的因素：不良饮食习惯、大量饮酒、吸烟等c.不可变的因素：年龄、性别、家族史等',
-        isAcrticle:1,
-        tag:'科普读物',
-        isCollected:0,
-      },
-      {
-        newsId:2,
-        title:'脑卒中后康复训练1(上肢)',
-        imgUrl:'https://s2.loli.net/2023/04/15/Bolacp5h87jxr3P.png',
-        content:' 本视频将带领病患依次练习肢体移动、翻身、起坐、站立、上下轮椅、扶物行走、抓握物品等患侧肢体的功能锻炼。',
-        isAcrticle:0,
-        tag:'肢体训练',
-        isCollected:1,
-      },
-      {
-        newsId:3,
-        title:'脑卒中治疗成功后如何进行语言训练与恢复',
-        imgUrl:'https://s2.loli.net/2023/04/15/CXzg7H8sv3yNr45.jpg',
-        content:' 必须尽早进行发音训练。先诱导发音，然后再说常用单字，如吃、喝、好、行等，或出示卡片，让病人读出上面的字。再依次教双音词、短语、短句、长句等。可结合说、视、听同时进行，如让病人穿衣服，则必须既说“穿衣服”让病人听，又要指着准备好的衣服，并作出穿衣服手势示意让病人看，让病人一边说穿衣服，一边练习自己穿衣服。',
-        isAcrticle:1,
-        tag:'语言训练',
-        isCollected:1,
-      },
-      {
-        newsId:4,
-        title:'脑卒中后康复训练——下肢训练',
-        imgUrl:'https://s2.loli.net/2023/04/15/4MQCwatriefmRG8.png',
-        content:' 中风是一种缺乏血液流向大脑部分部位的医疗状况，这可能回导致手臂和腿的无力。照顾者可以使用这个视频在家庭环境中对中风患者进行简单的练习。',
-        isAcrticle:0,
-        tag:'下肢训练',
-        isCollected:0,
-      },
-    ],
-    newsList:[],
+    news:[],
+    userId:'lsfjds525',
+    postid:0
   },
-  tabSelect(e) {
+  /**
+   * 根据tab序号获取内容
+   */
+  getNameFromTab(num){
+    
+  },
+  /**
+   * 获取资讯列表
+   */
+  getNews(){
+    
+    // console.log(this.data.type)
+    // console.log(this.data.userId)
     
     wx.request({
-      url: 'http://38980ab1.r6.vip.cpolar.cn/recovery/api/article/get_list',
+      url:baseUrl+'/article/get_list',
+      
       method:'POST',
       data:{
-        userid:'lsfjds525',
-        class_:'科普',
+        class_:this.data.class[this.data.TabCur],
+        userid:this.data.userId
       },
+      
+      
       success:(res)=>{
-        console.log(res)
+
+        console.log(res),
+        //转存到news[]
         this.setData({
-        newsList:res.data
+          news:res.data,
+
         })
-        console.log(this.data.newsList)
+      },
+      fail(){
+        console.log('获取失败')
+        wx.showToast({
+          title: '暂无内容',
+          icon:null
+        })
+      },
+      complete(){
+        
+        
+       
       }
     })
+    
 
+
+  },
+  /**
+   * 切换tab
+   */
+  tabSelect(e) {
+    //console.log(e.currentTarget.dataset.id)
+    this.getNameFromTab(e.currentTarget.dataset.id)
     this.setData({
+      
       TabCur: e.currentTarget.dataset.id,
       scrollLeft: (e.currentTarget.dataset.id-1)*60
-    })
+      
+    }),
+    //console.log(this.data.class)
+    //切换tab时获取资讯
+    this.getNews()
   },
   goTo:function(){
     wx.navigateTo({
       url: '../question/index',
     })
   },
+  /**
+   * 资讯详情 跳转到article/index
+   */
   goToDetail:function(e){
+    //console.log(e.currentTarget.id)
     wx.navigateTo({
-      url: '../article/index?id='+e.currentTarget.id,
+      url: '../article/index?id='+e.currentTarget.id
     })
   },
   goToFood:function(){
@@ -95,7 +109,21 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
-
+    // //获取userid
+    // var that = this;
+    // // 从用户缓存中获取数据
+    // wx.getStorage({
+    //   key: 'userInfo',
+    //   success (res) {
+    //     console.log(res.data);
+    //     that.setData({
+          
+    //       userId:res.data.userId
+    //     });
+    //   },
+    // })
+    
+    this.getNews()
   },
 
   /**
