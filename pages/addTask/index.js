d19c3ae15a828afd38064d97f49a271e34006f2d
wwq <1980409739@qq.com>
2023-07-06 11:26:29 +0800

    dateformat + 添加任务

--- pages/addTask/index.js
+++ pages/addTask/index.js
@@ -1,4 +1,10 @@
 // pages/addTask/index.js
+const app = getApp();
+const baseUrl=app.globalData.baseUrl
+//recovery/api/task/add
+
+import {dateFormat} from '../../utils/dateformat/dateformat'
+
 Page({
 
   /**
@@ -6,26 +12,117 @@ Page({
    */
   data: {
     pIndex: null,
-    patients: ['李淑芬', '王国发', '赵书'],
+    patients: [
+      {
+        name:'李淑芬',
+        patientId:'lsfjds525'
+      },
+      {
+        name:'李强',
+        patientId:'lqus3522dhs'
+      }
+    ],
     dIndex:null,
     difficulty:['易','中','难'],
-    date: '2023-7-18',
+    selectedDif:0,
+    date:null,
+    doctorId:'zzp1568789965',
+    //当前选择用户
+    selectedPatient:'',
+    circle_time:'',
+    type:'',
+    description:''
+  },
+  /**
+   * 添加task
+   * 
+   */
+  addTask(){
+    
+    wx.request({
+      url: baseUrl+'/task/add',
+      method:'POST',
+      data:{
+        doctor:this.data.doctorId,
+        patient:this.data.selectedPatient,
+        description:this.data.description,
+        deadline:dateFormat(this.data.date).format('YYYY-MM-DD'),
+        circle_time:this.data.circle_time,
+        type:this.data.type,
+        difficulty:this.data.selectedDif
+      },
+      success:(res)=>{
+        wx.showToast({
+          title: '发布成功',
+        })
+      }
+    })
   },
   DateChange(e) {
+    var nn=new Date(this.data.date)
+    //nn=dateFormat(nn).format('YYYY-MM-DD')
+    nn=dateFormat(this.data.date).format('YYYY-MM-DD')
+    
+    console.log(nn)
+
     this.setData({
       date: e.detail.value
     })
   },
+  /**
+   * 患者选择
+   */
   PickerChange(e) {
-    console.log(e);
+    //console.log(e);
     this.setData({
-      pIndex: e.detail.value
+      pIndex: e.detail.value,
+      selectedPatient:this.data.patients[e.detail.value].patientId
+    })
+    //console.log(this.data.patients[e.detail.value].patientId)
+  },
+  /**
+   * 难度选择
+   */
+  PickerChange1(e) {
+    //console.log(e);
+    this.setData({
+      dIndex: e.detail.value,
+      selectedDif:e.detail.value
     })
   },
-  PickerChange1(e) {
-    console.log(e);
+  /**
+   * 医生获取与自己绑定的患者，用于发布任务时选择患者
+   */
+  getMyPatient(){
+
+    //this.setdata({
+    //  patients:res.data  
+    //})
+
+  },
+  /**
+   * 周期输入监听
+   */
+  circleinput(e){
+    //console.log(e)
     this.setData({
-      dIndex: e.detail.value
+      circle_time:e.detail.value
+    })
+  },
+  /**
+   * 任务type输入监听
+   */
+  typeinput(e){
+    this.setData({
+      type:e.detail.value
+    })
+  },
+  /**
+   * 任务详情监听
+   */
+  describeinput(e){
+    this.setData({
+      description:e.detail.value
     })
   },
 
@@ -33,6 +130,24 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    //从缓存读医生id
+    // this.setData({
+    //   doctorId:
+      
+    // })
+
+    //获取当前日期
+    var today=dateFormat(new Date()).format('YYYY-MM-DD')
+    //console.log(month)
+    this.setData({
+      date:today
+      
+    })
+    
+    
+
+    //获取医生对应患者列表
+    this.getMyPatient()
 
   },
 
--- pages/index/index.js
+++ pages/index/index.js
@@ -1,9 +1,21 @@
 // index.js
 // 获取应用实例
 import * as echarts from '../../ec-canvas/echarts';
+/**
+ * 黄烨五月十五 2023/7/4 9:13:12
+  
+  /recovery/api/task/update
+  /recovery/api/task/get
+  /recovery/api/task/delete
 
+
+ */
 const app = getApp();
+const baseUrl=app.globalData.baseUrl
 
+/**
+ * 任务完成图表
+ */
 function initChart(canvas, width, height, dpr) {
   const chart = echarts.init(canvas, null, {
     width: width,
@@ -68,90 +80,120 @@ Page({
     ec: {
       onInit: initChart
     },
-    isPatient:1,
+    //1患者，0医生
+    isPatient:0,
+    doctorId:'255455mo',
+    patientId:'',
     colors:['bg-red','bg-blue','bg-green'],
     btn:['去接受','去打卡','已完成'],
     index:0,
     activeNames: ['1'],
     taskList:[
-      {
-        taskId:1,
-        url:'https://s2.loli.net/2023/04/17/mbhk3zOcYalJE1Q.jpg',
-        doctor:'赵医生',
-        time:'2023年4月10日 9:31',
-        ddl:'2023年5月5日',
-        name:'步行练习',
-        diff:'难',
-        done:0,
-        all:'30天',
-        status:0,
-        detail:'',
-      },
-      {
-        taskId:2,
-        url:'https://s2.loli.net/2023/04/17/mbhk3zOcYalJE1Q.jpg',
-        doctor:'赵医生',
-        time:'2023年4月1日 14:24',
-        ddl:'2023年4月28日',
-        name:'抬臂练习',
-        diff:'中',
-        done:11,
-        all:'20天',
-        status:1,
-        detail:'',
-      },
-      {
-        taskId:3,
-        url:'https://s2.loli.net/2023/04/17/mbhk3zOcYalJE1Q.jpg',
-        doctor:'赵医生',
-        time:'2023年3月3日 10:47',
-        ddl:'2023年4月6日',
-        name:'抬腿练习',
-        diff:'易',
-        done:30,
-        all:'30天',
-        status:2,
-        detail:'',
-      }
+      // {
+      //   taskId:1,
+      //   url:'https://s2.loli.net/2023/04/17/mbhk3zOcYalJE1Q.jpg',
+      //   doctor:'赵医生',
+      //   time:'2023年4月10日 9:31',
+      //   ddl:'2023年5月5日',
+      //   name:'步行练习',
+      //   diff:'难',
+      //   done:0,
+      //   all:'30天',
+      //   status:0,
+      //   detail:'',
+      // },
+      // {
+      //   taskId:2,
+      //   url:'https://s2.loli.net/2023/04/17/mbhk3zOcYalJE1Q.jpg',
+      //   doctor:'赵医生',
+      //   time:'2023年4月1日 14:24',
+      //   ddl:'2023年4月28日',
+      //   name:'抬臂练习',
+      //   diff:'中',
+      //   done:11,
+      //   all:'20天',
+      //   status:1,
+      //   detail:'',
+      // },
+      // {
+      //   taskId:3,
+      //   url:'https://s2.loli.net/2023/04/17/mbhk3zOcYalJE1Q.jpg',
+      //   doctor:'赵医生',
+      //   time:'2023年3月3日 10:47',
+      //   ddl:'2023年4月6日',
+      //   name:'抬腿练习',
+      //   diff:'易',
+      //   done:30,
+      //   all:'30天',
+      //   status:2,
+      //   detail:'',
+      // }
     ],
     patientList:[
-      {
-        id:1,
-        patientImg:'https://s2.loli.net/2023/04/14/vZo9rCgjNGIPSb2.jpg',
-        patientName:'李淑芬',
-        patientId:'2789257855',
-        taskList:[
-          {taskName:'抬臂练习',},
-          {taskName:'抬腿练习',},
-          {taskName:'步行练习',},
-        ]
-      },
-      {
-        id:2,
-        patientImg:'https://s2.loli.net/2023/04/18/AQGDHMYIja9Nd2g.jpg',
-        patientName:'赵书',
-        patientId:'2032578554',
-        taskList:[
-          {taskName:'抬臂练习',},
-          {taskName:'抬腿练习',},
-          {taskName:'步行练习',},
-        ]
-      },
-      {
-        id:3,
-        patientImg:'https://s2.loli.net/2023/04/14/SfTo1UYsOCZwBM2.jpg',
-        patientName:'王国发',
-        patientId:'2357846781',
-        taskList:[
-          {taskName:'抬臂练习',},
-          {taskName:'抬腿练习',},
-          {taskName:'步行练习',},
-        ]
-      },
+      // {
+      //   id:1,
+      //   patientImg:'https://s2.loli.net/2023/04/14/vZo9rCgjNGIPSb2.jpg',
+      //   patientName:'李淑芬',
+      //   patientId:'2789257855',
+      //   taskList:[
+      //     {taskName:'抬臂练习',},
+      //     {taskName:'抬腿练习',},
+      //     {taskName:'步行练习',},
+      //   ]
+      // },
+      // {
+      //   id:2,
+      //   patientImg:'https://s2.loli.net/2023/04/18/AQGDHMYIja9Nd2g.jpg',
+      //   patientName:'赵书',
+      //   patientId:'2032578554',
+      //   taskList:[
+      //     {taskName:'抬臂练习',},
+      //     {taskName:'抬腿练习',},
+      //     {taskName:'步行练习',},
+      //   ]
+      // },
+      // {
+      //   id:3,
+      //   patientImg:'https://s2.loli.net/2023/04/14/SfTo1UYsOCZwBM2.jpg',
+      //   patientName:'王国发',
+      //   patientId:'2357846781',
+      //   taskList:[
+      //     {taskName:'抬臂练习',},
+      //     {taskName:'抬腿练习',},
+      //     {taskName:'步行练习',},
+      //   ]
+      // },
     ]
   },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+    //获取当前医生id
+    
+    //获取医生任务列表
+    wx.request({
+      url: baseUrl+'/task/get',
+      method:'POST',
+      data:{
+        doctor:this.data.doctorId
 
+      },
+      success:(res)=>{
+        console.log(res),
+        this.setData({
+          patientList:res.data
+        })
+      }
+    })
+
+
+  },
+  /**
+   * 
+   */
   onReady() {
+
   },
   onShow(){
     this.tabBar();
