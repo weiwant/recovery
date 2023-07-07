0f897494bb028fff7d86abaedc0f9d25e08d8a90
ZWForum <782552186@qq.com>
2023-07-07 15:09:33 +0800

    init

--- src/models/correct/utils/config.py
+++ src/models/correct/utils/config.py
@@ -8,7 +8,6 @@ def get_config():
     :return:
     """
     parser = argparse.ArgumentParser()
-    parser.add_argument('--iter', type=int, default=10, help='number of iterations')
     parser.add_argument('--step', type=float, default=0.1, help='number of steps')
     parser.add_argument('--save-path', type=str, default='models/correct', help='path to save processed data')
     kwargs = vars(parser.parse_args())
