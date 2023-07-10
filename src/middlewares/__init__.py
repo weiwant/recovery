2285287b00a439cf3961e1e857a6f0123a84c4ce
ZWForum <782552186@qq.com>
2023-07-10 10:58:25 +0800

    init

--- src/middlewares/__init__.py
+++ src/middlewares/__init__.py
@@ -1,2 +1,8 @@
-REQUEST = []
-RESPONSE = []
+from src.middlewares.api import access_log_middleware, response_log_middleware
+
+REQUEST = [
+    access_log_middleware
+]
+RESPONSE = [
+    response_log_middleware
+]
---
+++ src/middlewares/api.py
@@ -0,0 +1,19 @@
+from sanic import Request
+
+from src.utils.logger import get_logger
+
+logger = get_logger(__name__)
+
+
+async def access_log_middleware(request: Request):
+    """
+    记录访问日志
+    """
+    logger.info(f'访问: {request.path}')
+
+
+async def response_log_middleware(request: Request, response):
+    """
+    记录响应日志
+    """
+    logger.info(f'响应: {request.path} {response.status}')
