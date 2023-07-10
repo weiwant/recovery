94ee9288ee3c6be9724b1cd45b50d8a568c8a047
wwq <1980409739@qq.com>
2023-07-10 15:49:54 +0800

    帖子评论

--- pages/circleDetail/index.js
+++ pages/circleDetail/index.js
@@ -11,57 +11,13 @@ Page({
     postId:'',
     userId:'lsfjds525',
     detail:[],
-    isGood:0
+    isGood:0,
     
-    // index:0,
-    // iconName:['cuIcon-appreciatefill','cuIcon-appreciate'],
-    // tt:['已赞(884)','点赞(883)'],
-    // userImg:'https://s2.loli.net/2023/04/14/boRaWVHMzskLc6B.jpg',
-    // userName:'疗康院康复师9527',
-    // isDoctor:1,
-    // time:'2023年4月3日 14:44',
-    // content:'&emsp;&emsp;今天很荣幸参加了由GZCPT主办的脑卒中康复治疗学习班，“镜”观其变，以“镜”促动--带您走进脑卒中后康复的镜像世界了解到了新镜子疗法。在这里把一些要点分享给大家。\n&emsp;&emsp;镜像疗法是基于镜像神经元理论，利用大脑的可塑性,通过视觉反馈、运动观察、运动想象、运动模仿来进行康复训练的一种治疗手段。根据镜面反射相等的物象,以正常肢体镜像代替患侧肢体。患者通过这样视觉反馈进行运动观察，模仿以及再学习。',
-    // view:1684,
-    // isGood:0,
-    // good:883,
-    // comment:307,
-    // imgList:[
-    //   {url:'https://s2.loli.net/2023/04/14/nMYUPyKBTxjgqSG.png'},
-    //   {url:'https://s2.loli.net/2023/04/14/VkqXaetJ3cGCnAU.png'},
-    //   {url:'https://s2.loli.net/2023/04/14/ksPzQiIdbVg5rZm.png'},
-    // ],
-    // commentList:[
-    //   {
-    //     userUrl:'https://s2.loli.net/2023/04/26/NZctY6iLEuHvpAI.jpg',
-    //     userName:'喜洋洋',
-    //     content:'从我的个人经验来说 我认为这个训练是有用的。。',
-    //     time:'2023年4月5日 08:21',
-    //     isGood:1,
-    //     children:[
-    //       {
-    //         username:'疗康院康复师9527',
-    //         cont:'贵在坚持，会有效果的！✊'
-    //       },
-    //       {
-    //         username:'咪宝的阿婆',
-    //         cont:'：训练了多久哇'
-    //       }
-    //     ]
-    //   },
-    //   {
-    //     userUrl:'https://s2.loli.net/2023/04/26/SVwHirl9U63aQyz.jpg',
-    //     userName:'心渡',
-    //     content:'真的有用吗？可以再说详细一点吗？',
-    //     time:'2023年4月4日 18:17',
-    //     isGood:0,
-    //     children:[
-    //       {
-    //         username:'疗康院康复师9527',
-    //         cont:'：可以看我的最新博文🙂'
-    //       },
-    //     ]
-    //   },
-    // ]
+    //评论内容
+    comment:'',
+    parent:null,
+    
+    
   },
   /**
    * 获取帖子详情
@@ -88,10 +44,50 @@ Page({
   /**
    * 点赞
    */
-  change:function(){
-    this.setData({
-      index: (this.data.index + 1) % 2
-    })
+  change:function(e){
+    console.log(e)
+    //点赞数
+    var stars="detail.stars"
+    //传递后端
+    if(1!=this.data.isGood){
+      //点赞
+      this.setData({
+        isGood:1,
+        //点赞数
+        [stars]:this.data.detail.stars+1
+      })
+      wx.request({
+        url: baseUrl+'/posts/like',
+        method:'POST',
+        data:{
+          userid:this.data.userId,
+          post_id:this.data.postId
+          
+        },
+        success:(res)=>{
+          console.log(res)
+        }
+      })
+    } else {
+      //取消点赞
+      this.setData({
+        isGood:0,
+        //点赞数
+        [stars]:this.data.detail.stars-1
+      })
+      wx.request({
+        url: baseUrl+'/posts/dislike',
+        method:'POST',
+        data:{
+          userid:this.data.userId,
+          post_id:this.data.postId
+          
+        },
+        success:(res)=>{
+          console.log(res)
+        }
+      })
+    }
   },
   showModal(e) {
     this.setData({
@@ -104,6 +100,58 @@ Page({
     })
   },
   /**
+   * 输入框内容获取
+   */
+  commentInput(e){
+    //console.log(e)
+    this.setData({
+      comment:e.detail.value
+    })
+  },
+  /**
+   * 普通评论
+   */
+  say(){
+    wx.request({
+      url: baseUrl+'/posts/comment',
+      method:'POST',
+      data:{
+        commenter:this.data.userId,
+        post_id:this.data.postId,
+        parent:this.data.parent,
+        content:this.data.comment
+
+      },
+      success:(res)=>{
+        console.log(res)
+        wx.showToast({
+          title: '评论成功',
+        })
+        this.setData({
+          comment:''
+        })
+        this.onShow()
+      }
+    })
+    
+  },
+  /**
+   * 子评论
+   */
+  sonSay(e){
+    //获取父评论id
+    console.log(e)
+    var fComment=e.currentTarget.dataset.commentid
+    this.setData({
+      parent:fComment
+    })
+    this.say()
+    this.setData({
+      parent:null
+    })
+
+  },
+  /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
@@ -113,6 +161,7 @@ Page({
       isGood:options.isgood
     });
     this.getPostDetail()
+    
   },
 
   /**
@@ -126,7 +175,7 @@ Page({
    * 生命周期函数--监听页面显示
    */
   onShow() {
-
+    this.getPostDetail()
   },
 
   /**
