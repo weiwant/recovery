06e7ac9924b62c63030ba009eeac43a48502f270
ZWForum <782552186@qq.com>
2023-07-10 10:40:26 +0800

    init

--- config/__init__.py
+++ config/__init__.py
@@ -14,7 +14,13 @@ TABLES = {
     'task_info': 'id',
     'patient_info': 'id',
     'doctor_info': 'id',
-    'detail_info': 'id'
+    'detail_info': 'id',
+    'user_bind': 'id',
+    'articles': 'id',
+    'comments': 'id',
+    'posts': 'id',
+    'likes': 'id',
+    'collect': 'id'
 }
 USE_GPU = True
 OPENPOSE_ROOT = 'openpose-1.7.0'
